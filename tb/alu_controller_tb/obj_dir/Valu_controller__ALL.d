4fb29092e6c152bc59183e2df8bddbe3d47e0bcc
Morten Sørensen <morten.sorensen.04@gmail.com>
2024-12-31 01:37:58 +0100

    cleanup

--- tb/alu_controller_tb/obj_dir/Valu_controller__ALL.d
+++
@@ -1,8 +0,0 @@
-Valu_controller__ALL.o: Valu_controller__ALL.cpp Valu_controller.cpp \
- Valu_controller.h /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Valu_controller__Syms.h \
- Valu_controller__Trace.cpp \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h Valu_controller__Slow.cpp \
- Valu_controller__Syms.cpp Valu_controller__Trace__Slow.cpp
